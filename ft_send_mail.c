a9706ea82de849cfdc1a74db5c5fb9d580354442
Guillaume Roubaud <groubaud@fu-r7-p7.s19.be>
2021-11-05 19:03:15 +0100

    deleted useless comment#

--- ft_send_mail.c
+++ ft_send_mail.c
@@ -6,16 +6,13 @@
 /*   By: groubaud <groubaud@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/04 15:13:35 by groubaud          #+#    #+#             */
-/*   Updated: 2021/11/05 18:55:14 by groubaud         ###   ########.fr       */
+/*   Updated: 2021/11/05 19:02:35 by groubaud         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_secret_santa.h"
 #include <stdio.h>
 #include <stdlib.h>
-
-// if there is a day when I'm capable to send mail in command line
-// it will be call in this function
 
 void	ft_mail(char *mail_user, char *mail_content)
 {
@@ -25,6 +22,7 @@ void	ft_mail(char *mail_user, char *mail_content)
 	fd = fopen("secret_santa.log", "a+");
 	fprintf(fd, "Send mail to : %s\ncontent : |%s|\n\n",
 			mail_user, mail_content);
+
 	sprintf(cmd, "echo %s | mail -s %s %s",
 				mail_content,
 				"TEST_secret_santa_TEST",
@@ -42,7 +40,6 @@ int ft_send_mail(int *tab_even, char **tab_pseudo, int len)
 	char	mail_content[30];
 
 	i = 0;
-	
 	while (i < len)
 	{
 		sprintf(mail_user, "%s@student.s19.be", tab_pseudo[i]);
